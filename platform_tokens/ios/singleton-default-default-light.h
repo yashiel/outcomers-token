69edda4de5e64c53224dd10d70e66148e84c3fd5
Yashi <hi@yashiel.com>
2024-01-16 11:23:46 +0530

    added default folder

---
+++ platform_tokens/ios/singleton-default-default-light.h
@@ -0,0 +1,19 @@
+
+//
+// ios/singleton-default-default-light.h
+//
+
+// Do not edit directly
+// Generated on Tue, 16 Jan 2024 05:53:17 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface  : NSObject
+
++ (NSDictionary *)properties;
++ (NSDictionary *)getProperty:(NSString *)keyPath;
++ (nonnull)getValue:(NSString *)keyPath;
+
+@end
