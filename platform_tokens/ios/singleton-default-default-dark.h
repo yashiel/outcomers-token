e089d5a7499945281f70878d98161f4b3b943f4e
yashiel <yashiel@users.noreply.github.com>
2024-01-16 13:56:23 +0000

    Update Design Tokens

---
+++ platform_tokens/ios/singleton-default-default-dark.h
@@ -0,0 +1,19 @@
+
+//
+// ios/singleton-default-default-dark.h
+//
+
+// Do not edit directly
+// Generated on Tue, 16 Jan 2024 13:56:22 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface  : NSObject
+
++ (NSDictionary *)properties;
++ (NSDictionary *)getProperty:(NSString *)keyPath;
++ (nonnull)getValue:(NSString *)keyPath;
+
+@end
